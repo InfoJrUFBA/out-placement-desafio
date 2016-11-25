b609c57533b51ee7c51ac7440ebe882ac333c60a
Gabriel Goulart <gabrielgoulart@infojr.com.br>
2016-11-25 18:09:14 -0300

    Adiciona helper de emailiI

---
+++ app/helpers/mailer.php
@@ -0,0 +1,25 @@
+<?php
+    require_once('../vendors/class.phpmailer.php');
+    class Mailer {
+        public static function sendEmail($msg, $email, $topic){
+            foreach (file('../.email-config') as $line) {
+                list($key, $value) = explode(':', $line, 2) + [NULL, NULL];
+                $conf[trim($key)] = trim($value);
+            }
+
+            $mail = new PHPMailer(true);
+            $mail->IsSMTP();
+            $mail->SMTPAuth = true;
+            $mail->SMTPSecure = $conf['SMTPSecure'];
+            $mail->Host = $conf['Host'];
+            $mail->Port = $conf['Port'];
+            $mail->Username = $conf['Email'];
+            $mail->Password = $conf['Password'];
+            $mail->AddAddress($email);
+            $mail->SetFrom($conf['Email'], 'Recuperação de Senha');
+            $mail->Subject = $topic;
+            $mail->MsgHTML($msg);
+            $mail->Send();
+        }
+    }
+?>
