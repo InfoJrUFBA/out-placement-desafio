a16253deb0699a5e312dc66a09e225ef9469a3ef
Mateus <mateusbarbosa@infojr.com.br>
2016-11-25 17:01:55 -0300

    Adiciona home

---
+++ app/views/home.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
+<header>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>barras
+                <ul>
+                    <li>Perfil</li>
+                    <li>Sair</li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+</header>
+<section>
+    <h4>Hoje</h4>
+        <div class="card">
+
+        </div>
+    <h4>2016</h4>
+    <div>
+
+    </div>
+    <h4>2015</h4>
+    <div>
+
+    </div>
+</section>
+</body>
+</html>
