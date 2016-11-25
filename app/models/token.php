d0e5c167daa8c90df34139b1512ffb9d86444222
Gabriel Goulart <gabrielgoulart@infojr.com.br>
2016-11-25 18:25:38 -0300

    Adiciona model de token

--- app/models/token.php
+++ app/models/token.php
@@ -1,24 +1,24 @@
 <?php
     class Token extends Connection;
-    private $idtoken;
-    private $users_id;
-    private $expiration_date;
+    private $token;
+    private $user_id;
+    private $expiration;
 
     public function __construct($email, $id, $date){
         $this->token = md5($email . $date);
-        $this->users_id = $id;
-        $this->expiration_date = $date;
+        $this->user_id = $id;
+        $this->expiration = $date;
     }
 
     public static function createToken($attributes){
         $email = array_key_exists('email', $attributes) ? $attributes['email'] :: null;
         if($email) {
-            $user_id = User::getId($email);
-            if($id) {
+            $userid = User::getId($email);
+            if($userid) {
                 $date = new DateTime();
                 $date->add(new DateInterval(PT10M));
                 $time = $date->format('Y-m-d H:i:s');
-                $token = new Token($email, $user_id, $time);
+                $token = new Token($email, $userid, $time);
                 return $token;
             }
             return 0;
@@ -28,5 +28,9 @@
 
     public static function insert(){
         $connection = self::connect;
-
+        $stm = $connection->prepare('INSERT INTO recovery(token, expiration, user_id) VALUES (:token, :expiration, :userid)');
+        $stm->BindValue(":token", $this->token, PDO::PARAM_STR);
+        $stm->BindValue(":expiration", $this->token, PDO::PARAM_INT);
+        $stm->BindValue(":userid", $this->token, PDO::PARAM_STR);
+        return $stm->execute();
     }
