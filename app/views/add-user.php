9f98c0c5c6c40a2853fee5729bc8055590a981e3
Jurgen Fink <jurgenfink@infojr.com.br>
2016-11-25 16:05:30 -0300

    Cria mais pastas e começa add-user.

--- app/views/add-user.php
+++ app/views/add-user.php
@@ -1,3 +1,14 @@
-<?php
-
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link type="text/css" rel="stylesheet" href="../assets/css/stylesheets.css">
+        <title>Criar Usuário</title>
+    </head>
+    <body>
+        <div>
+            <h1> Criar Usuário </h1>
+            <?php include('includes/user-form.inc'); ?>
+        </div>
+    </body>
+</html>
