d9bdd5881e614e2b16fd822f021254ee5fa6e747
Gabriele Luz <gabrieleluz@infojr.com.br>
2016-11-25 18:07:23 -0300

    Começa front de página de perfil.

---
+++ app/views/user-profile.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link type="text/css" rel="stylesheet" href="../assets/css/stylesheets.css">
+        <title>Perfil</title>
+    </head>
+    <body>
+        <div class="container" id="profile_box">
+            <div id="profile_top">
+            	<div id="profile_photo"></div>
+            </div>
+            <div id="profile_bot">
+            	
+            </div>
+        </div>
+    </body>
+</html>
